0577f482d311fbf03ae43e3116da1bc969c445a8
pachungulo <pachungulo@gmail.com>
2024-07-04 21:37:17 -0400

    Add automatic server lsp through mason-lspconfig
    Add functionality to mason-lspconfig so I no longer have to setup each
    lsp manually

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -11,13 +11,17 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = { "lua_ls" }
       })
+      require("mason-lspconfig").setup_handlers {
+        function (server_name) 
+          require("lspconfig")[server_name].setup {}
+        end,
+      } 
     end
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
