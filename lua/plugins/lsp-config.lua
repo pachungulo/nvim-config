97113ee050e1d19b7f837bbe5a6465a8a8daaeda
pachungulo <pachungulo@gmail.com>
2024-07-04 21:53:26 -0400

    Code cleanup and add new bindings
    Added new keybindings to navigate between windows easier in nvim, and
    remove whitespaces because the lsp was complaining lol.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,10 +12,10 @@ return {
         ensure_installed = { "lua_ls" }
       })
       require("mason-lspconfig").setup_handlers {
-        function (server_name) 
+        function (server_name)
           require("lspconfig")[server_name].setup {}
         end,
-      } 
+      }
     end
   },
   {
