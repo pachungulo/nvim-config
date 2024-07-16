af179ae44051e1fac2aeb3e8a3e1c50d3222b6ad
pachungulo <115584328+pachungulo@users.noreply.github.com>
2024-07-16 00:17:29 -0400

    Modify telescope.nvim and none-ls keybindings
    Modify telescope and none-ls keybindigs to be inline with kickstart/stup's keybindigs. Typecraft's bindings suck imo because they seem random and overly rely on g.

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,6 +9,6 @@ return {
       },
     })
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format buffer" })
+    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "[C]ode [F]ormat" })
   end,
 }
