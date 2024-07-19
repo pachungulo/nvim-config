6ceb8172f07663e8c573b663cadc65492028ec9f
pachungulo <115584328+pachungulo@users.noreply.github.com>
2024-07-18 22:44:30 -0400

    Add gitsigns to config
    Added gitsigns plugin to config plus the preview hunk keybind, which I came up with myself teehee.

---
+++ lua/plugins/gitstuff.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+      vim.keymap.set("n", "<leader>ph", ":Gitsigns preview_hunk<CR>", { desc = "[P]review [H]unk" })
+    end,
+  },
+  {},
+}
