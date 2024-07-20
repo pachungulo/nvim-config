70114ccffc2494d27c64bbf20dc0baae923571d3
pachungulo <115584328+pachungulo@users.noreply.github.com>
2024-07-20 18:16:03 -0400

    Improve git functionality
    Added vim-fugitive and some keybinds to Gitsigns to improve workflow and
    never have to exit neovim for git bs ever again.

--- lua/plugins/gitstuff.lua
+++ lua/plugins/gitstuff.lua
@@ -4,7 +4,11 @@ return {
     config = function()
       require("gitsigns").setup()
       vim.keymap.set("n", "<leader>ph", ":Gitsigns preview_hunk<CR>", { desc = "[P]review [H]unk" })
+      vim.keymap.set("n", "[c", ":Gitsigns prev_hunk<CR>", { desc = "Previous Hunk" })
+      vim.keymap.set("n", "]c", ":Gitsigns next_hunk<CR>", { desc = "Next Hunk" })
     end,
   },
-  {},
+  {
+    "tpope/vim-fugitive",
+  },
 }
