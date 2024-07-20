dbde1c0ae968467086a0e0877850db3f9d1fd649
pachungulo <115584328+pachungulo@users.noreply.github.com>
2024-07-20 16:27:27 -0400

    Fixed formatting bug
    Fixed bug caused when both the lsp and none-ls have formatters available for the filetype. Before, the formatter chosen would depend on which was loaded first, which is non-deterministic. Now, null-ls is prioritized if it has a formatter setup, else the lsp will be used.

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,3 +1,18 @@
+local lsp_formatting = function()
+  vim.lsp.buf.format({
+    filter = function(client)
+      -- apply whatever logic you want (in this example, we'll only use null-ls)
+      local null_ls_sources = require("null-ls.sources")
+      local null_ls_formatting_sources = null_ls_sources.get_available(vim.bo.filetype, "NULL_LS_FORMATTING")
+      local null_ls_formatting_available = #null_ls_formatting_sources ~= 0
+      if null_ls_formatting_available then
+        return client.name == "null-ls"
+      else
+        return true
+      end
+    end,
+  })
+end
 return {
   "nvimtools/none-ls.nvim",
   config = function()
@@ -5,10 +20,9 @@ return {
 
     null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.stylua,
       },
     })
 
-    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "[C]ode [F]ormat" })
+    vim.keymap.set("n", "<leader>cf", lsp_formatting, { desc = "[C]ode [F]ormat" })
   end,
 }
