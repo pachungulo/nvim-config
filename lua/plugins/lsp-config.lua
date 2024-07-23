3ac9484e723a721358d10872d2cf45fbaaac4bd3
pachungulo <115584328+pachungulo@users.noreply.github.com>
2024-07-23 02:55:47 -0400

    Change how capabilities are given to lsp
    Now more like stup and kickstart.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -13,7 +13,8 @@ return {
       })
       require("mason-lspconfig").setup_handlers({
         function(server_name)
-          local capabilities = require("cmp_nvim_lsp").default_capabilities()
+          local capabilities = vim.lsp.protocol.make_client_capabilities()
+          capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
           require("lspconfig")[server_name].setup({
             capabilities = capabilities,
           })
