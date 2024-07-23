a28a4cf9eaa8dc76c915e5c5e04f6ab6b6231b6f
pachungulo <115584328+pachungulo@users.noreply.github.com>
2024-07-23 03:21:12 -0400

    Add completions keybind
    Can use C-l and C-h to navigate snippets.

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -33,6 +33,16 @@ return {
           ["<CR>"] = cmp.mapping.confirm({ select = false }),
           ["<Tab>"] = cmp.mapping.select_next_item(),
           ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ['<C-l>'] = cmp.mapping(function()
+            if require("luasnip").expand_or_locally_jumpable() then
+              require("luasnip").expand_or_jump()
+            end
+          end, { 'i', 's' }),
+          ['<C-h>'] = cmp.mapping(function()
+            if require("luasnip").locally_jumpable(-1) then
+              require("luasnip").jump(-1)
+            end
+          end, { 'i', 's' }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
