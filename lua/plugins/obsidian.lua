c08a4e629e48afb219d798d1ed48c1e49432942f
pachungulo <115584328+pachungulo@users.noreply.github.com>
2024-08-02 22:52:18 -0400

    Added markdown.nvim to config
    markdown.nvim renders markdown files in a way that is nicer than
    obsidian.nvim imo. However, it is a bit laggier. Had to disable obsidian
    ui.

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,6 +1,6 @@
 return {
   "epwalsh/obsidian.nvim",
-  version = "*",  -- recommended, use latest release instead of latest commit
+  version = "*", -- recommended, use latest release instead of latest commit
   lazy = true,
   ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
@@ -27,6 +27,7 @@ return {
       --   path = "~/vaults/work",
       -- },
     },
+    ui = { enable = false },
 
     -- see below for full list of options 👇
   },
