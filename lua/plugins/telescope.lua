b8b8634468e88f715192ed3294b948150164a8bd
pachungulo <115584328+pachungulo@users.noreply.github.com>
2024-09-01 22:07:50 -0400

    Redo fzf-native config to make less ambiguous

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,14 +4,16 @@ return {
     tag = "0.1.8",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      cond = function()
-        return vim.fn.executable "make" == 1
-      end,
-      config = function()
-        require('telescope').load_extension("fzf")
-      end
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        cond = function()
+          return vim.fn.executable "make" == 1
+        end,
+        config = function()
+          require('telescope').load_extension("fzf")
+        end,
+      },
     },
     config = function()
       local builtin = require("telescope.builtin")
