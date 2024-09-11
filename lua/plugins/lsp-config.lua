6a386ce062db63aa0a4818c5a267ca5ed2a1e986
pachungulo <115584328+pachungulo@users.noreply.github.com>
2024-09-10 23:02:53 -0400

    Fixed lua lsp vim global

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,6 +17,17 @@ return {
             capabilities = capabilities,
           })
         end,
+        ["lua_ls"] = function()
+          require("lspconfig").lua_ls.setup {
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" },
+                },
+              },
+            }
+          }
+        end
       })
     end,
   },
