11292e4bc928b46ae89b888ec2f0bae2f2a501bf
pachungulo <115584328+pachungulo@users.noreply.github.com>
2024-09-20 12:45:56 -0400

    Removed obsidian.lua
    Just using the obsidian app now

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -6,11 +6,5 @@ return {
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-  config = function()
-    local client = require("obsidian").get_client()
-    client.opts.ui.enable = false
-    local namespaceID = vim.api.nvim_get_namespaces()["ObsidianUI"]
-    vim.api.nvim_buf_clear_namespace(0,namespaceID,0,-1) -- Clear all the obsidian UI stuff before loading this.
-    require("render-markdown").setup({})
-  end
+  opts = {}
 }
