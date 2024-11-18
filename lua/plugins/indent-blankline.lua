b6b4377ab863914edb7e4a67032bf19a4635e394
pachungulo <115584328+pachungulo@users.noreply.github.com>
2024-11-17 23:41:24 -0500

    Added indent-blankline to config

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,12 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    opts = {
+        scope = {
+            show_start = false,
+            show_end = false,
+            highlight = "diffLine",
+        }
+    },
+}
