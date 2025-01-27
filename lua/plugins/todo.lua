d3e5ff9973ee540d2c40ed6e59ed6c38d2e370e0
pachungulo <115584328+pachungulo@users.noreply.github.com>
2025-01-26 22:15:09 -0500

    Added todo snacks picker

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -5,5 +5,9 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-  }
+  },
+  keys = {
+    { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
+    { "<leader>sT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
+  },
 }
